a71863b681da12495ee2f291bf2fe352dcfc283d
Eric Foy <foy.8@wright.edu>
2023-02-04 14:48:37 -0500

    now with timezone info

--- src/epcis_service/eventtime.py
+++ src/epcis_service/eventtime.py
@@ -2,6 +2,6 @@ import time
 from datetime import datetime, timezone
 
 def now():
-    return datetime.now().isoformat(timespec='seconds')
+    return datetime.utcnow().isoformat(timespec='seconds')
 
 #2005-04-03T20:33:31.116-06:00
