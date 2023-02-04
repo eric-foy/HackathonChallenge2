8f4e2e6e6810a286a28d0ab190c889a55e6d9351
Eric Foy <foy.8@wright.edu>
2023-02-04 15:28:53 -0500

    generation of observation events

--- src/epcis_service/generateevent.py
+++ src/epcis_service/generateevent.py
@@ -13,8 +13,6 @@ def genSampleEvent():
 
     ObjectEvent = ET.SubElement(EventList, 'ObjectEvent')
     ET.SubElement(ObjectEvent, 'eventTime').text = eventtime.now()
-    #ET.SubElement(ObjectEvent, 'eventTime').text = '2005-04-03T20:33:31.116-06:00'
-    ET.SubElement(ObjectEvent, 'eventTimeZoneOffset').text = '-06:00'
 
     epcList = ET.SubElement(ObjectEvent, 'epcList')
     ET.SubElement(epcList, 'epc').text = 'urn:epc:id:sgtin:0614141.107346.2017'
@@ -27,8 +25,34 @@ def genSampleEvent():
     return ET.tostring(root, encoding='utf8', method='xml')
     #return ET.tostring(root)
 
-def genObservationEvent():
-    pass
+def genObservationEvent(sgtin, sgln, quantity, uom):
+    rootArgs = {'xmlns:epcis':'urn:epcglobal:epcis:xsd:1',
+            'xmlns:example':'http://ns.example.com/epcis',
+            'xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance',
+            'schemaVersion':'1.2'}
+    root = ET.Element('epcis:EPCISDocument', rootArgs)
+
+    EPCISBody = ET.SubElement(root, 'EPCISBody')
+    EventList = ET.SubElement(EPCISBody, 'EventList')
+
+    ObjectEvent = ET.SubElement(EventList, 'ObjectEvent')
+    ET.SubElement(ObjectEvent, 'eventTime').text = eventtime.now()
+
+    epcList = ET.SubElement(ObjectEvent, 'epcList')
+    ET.SubElement(epcList, 'epc').text = 'urn:epc:id:sgtin:'+sgtin
+
+    ET.SubElement(ObjectEvent, 'action').text = 'OBSERVE'
+
+    bizLocation = ET.SubElement(ObjectEvent, 'bizLocation')
+    ET.SubElement(bizLocation, 'id').text = 'urn:epc:id:sgln:'+sgln
+
+    extension = ET.SubElement(ObjectEvent, 'extension')
+    quantityList = ET.SubElement(extension, 'quantityList')
+    quantityElement = ET.SubElement(quantityList, 'quantityElement')
+    ET.SubElement(quantityElement, 'quantity').text = quantity
+    ET.SubElement(quantityElement, 'uom').text = uom
+
+    return ET.tostring(root, encoding='utf8', method='xml')
 
 def genTransformationEvent():
     pass
--- src/epcis_service/main.py
+++ src/epcis_service/main.py
@@ -3,9 +3,41 @@ import generateevent
 
 #xml = '<?xml	version="1.0"	encoding="UTF-8"	standalone="yes"?>'
 #xml = xml + generateevent.genSampleEvent().decode("utf-8") 
-xml = generateevent.genSampleEvent()
+#xml = generateevent.genSampleEvent()
+
+serial = 0
+
+# lettuce
+gtin = '10810055931016'
+serial = serial + 1
+sgtin = gtin+'.'+str(serial)
+sgln = '0810055931231'
+quantity = '20'
+uom = 'each'
+xml = generateevent.genObservationEvent(sgtin, sgln, quantity, uom)
+capture.capture(xml)
+
+# tomatos
+gtin = '10810055931023'
+serial = serial + 1
+sgtin = gtin+'.'+str(serial)
+sgln = '0810055931231'
+quantity = '50'
+uom = 'LB'
+xml = generateevent.genObservationEvent(sgtin, sgln, quantity, uom)
+capture.capture(xml)
+
+# containers
+gtin = '10810055931030'
+serial = serial + 1
+sgtin = gtin+'.'+str(serial)
+sgln = '0810055931231'
+quantity = '500'
+uom = 'each'
+xml = generateevent.genObservationEvent(sgtin, sgln, quantity, uom)
+capture.capture(xml)
+
 
 #xml = xml + '<epcis:EPCISDocument xmlns:epcis="urn:epcglobal:epcis:xsd:1" xmlns:example="http://ns.example.com/epcis" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" schemaVersion="1.2"><EPCISBody><EventList><ObjectEvent> <eventTime>2005-04-03T20:33:31.116-06:00</eventTime><eventTimeZoneOffset>-06:00</eventTimeZoneOffset><epcList><epc>urn:epc:id:sgtin:0614141.107346.2017</epc></epcList><action>OBSERVE</action><readPoint><id>urn:epc:id:sgln:0614141.07346.1234</id></readPoint></ObjectEvent></EventList></EPCISBody></epcis:EPCISDocument>'
 
 #print(xml)
-capture.capture(xml)
